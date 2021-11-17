615d6e1a0b4044dedea80e20146a90d451777771
Stefan Felkel <16918854+tirolerstefan@users.noreply.github.com>
2021-11-17 19:58:15 +0100

    created missing directories before copying files (thanks @alexdpunkt)

--- dpkg/build_pkg.sh
+++ dpkg/build_pkg.sh
@@ -3,6 +3,9 @@
 cd content
 
 # copy files to debian package structure
+mkdir -p usr/lib/kaifareader/
+mkdir -p etc/kaifareader/
+mkdir -p lib/systemd/system/
 cp -v ../../kaifareader.py               usr/lib/kaifareader/
 cp -v ../../meter_template.json          etc/kaifareader/
 cp -v ../../systemd/kaifareader.service  lib/systemd/system/
